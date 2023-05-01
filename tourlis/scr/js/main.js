3b40be2195c5ec28ba2707f3c10f6cc4838b4a52
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2023-05-01 12:06:02 -0300

    Add files via upload

---
+++ tourlis/scr/js/main.js
@@ -0,0 +1,7 @@
+import bootstrap from "bootstrap"
+
+// ação para seção de usuário
+let dropdown = new bootstrap.Dropdown(document.getElementById('userDropdown'), {
+    // opções, se necessário
+  })
+  
